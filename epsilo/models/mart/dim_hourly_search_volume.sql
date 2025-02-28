89749f94d8f83fca9a92752e3943ca222a723644
Narius2030 <buiducnhan15122003@gmail.com>
2025-02-28 23:50:05 +0700

    feat: transform dimension and mart

--- epsilo/models/mart/dim_hourly_search_volume.sql
+++ epsilo/models/mart/dim_hourly_search_volume.sql
@@ -3,6 +3,6 @@
 ) }}
 
 SELECT 
-    {{ generate_surrogate_key(['created_datetime']) }} AS hourly_key, 
+    {{ generate_surrogate_key(["keyword_id", "created_datetime"]) }} AS hourly_key, 
     keyword_id, created_datetime, search_volume
 FROM {{ source("epsilo", "hourly_search_volume") }}
