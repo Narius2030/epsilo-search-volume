a8be5c3f490fe233260b94bc2a41872e55f89c71
Narius2030 <buiducnhan15122003@gmail.com>
2025-03-01 16:12:34 +0700

    feat: create dimension and mart

---
+++ epsilo/models/mart/dim_subscriptions.sql
@@ -0,0 +1,20 @@
+{{ config(
+    unique_key=["subscription_id],
+    depends_on=['stg_overlap_subscriptions']
+) }}
+
+WITH dimension AS (
+    SELECT
+        MD5(CONCAT(
+            COALESCE(CAST(s.user_id AS CHAR), ''),
+            '-',
+            COALESCE(CAST(s.keyword_id AS CHAR), ''),
+            '-',
+            COALESCE(CAST(s.subscription_id AS CHAR), '')
+        )) AS subscription_key,
+        s.subscription_id, s.user_id, s.keyword_id, s.timing,
+        s.start_time, s.end_time
+    FROM {{ ref("stg_overlap_subscriptions") }} s
+)
+
+SELECT * FROM dimension
---
+++ epsilo/models/staging/stg_overlap_subscription.sql
@@ -0,0 +1,36 @@
+{{ config(
+    unique_key="id"
+) }}
+
+USE epsilo;
+
+WITH non_overlap AS (
+    SELECT 
+        s1.user_id,
+        s1.keyword_id,
+        s1.timing,
+        MIN(s1.start_time) AS start_time,
+        MAX(s1.end_time) AS end_time,
+        (CASE
+			WHEN s1.start_time<=s2.start_time AND s1.end_time>=s2.end_time
+			THEN s1.subscription_id ELSE s2.subscription_id
+		END AS subscription_id)
+    FROM {{ source("epsilo", "subscriptions") }} s1
+    JOIN {{ source("epsilo", "subscriptions") }} s2 
+        ON s2.user_id=s1.user_id
+        AND s2.keyword_id=s1.keyword_id
+        AND s2.start_time<s1.end_time
+        AND s1.start_time<s2.end_time
+    GROUP BY s1.user_id, s1.keyword_id, s1.timing,
+        CASE
+            WHEN s1.start_time<=s2.start_time AND s1.end_time>=s2.end_time
+            THEN s1.subscription_id ELSE s2.subscription_id
+        END
+)
+
+SELECT 
+    user_id, keyword_id, subscription_id, timing,
+    MIN(start_time) AS start_time,
+    MAX(end_time) AS end_time
+FROM non_overlap
+GROUP BY user_id, keyword_id, subscription_id, timing,
